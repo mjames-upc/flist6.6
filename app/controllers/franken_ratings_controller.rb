2e6a05e1751319669bac803b13eb2e230a63949f
Prelang Builder <builder@prelang.com>
2015-02-09 17:44:51 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/franken_ratings_controller.rb
+++ app/controllers/franken_ratings_controller.rb
@@ -1,4 +1,8 @@
 class FrankenRatingsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_franken_rating, only: [:show, :edit, :update, :destroy]
 
   # GET /franken_ratings
